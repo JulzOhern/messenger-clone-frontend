9205d3529e6baf66d2255a690f79919455f96cc2
Julius <hjulius742002@gmail.com>
2026-05-14 00:26:29 +0800

    feat: add PWA support with vite-plugin-pwa and update project icons
    - Updated package.json to include vite-plugin-pwa.
    - Added new icons for PWA: android-chrome-192x192.png, android-chrome-512x512.png, apple-touch-icon.png, favicon-16x16.png, favicon-32x32.png, and favicon.ico.
    - Modified tsconfig.app.json for improved configuration and added ignoreDeprecations option.
    - Enhanced vite.config.ts to configure PWA settings and manifest.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      /* devOptions: {
+        enabled: true,
+        type: 'module'
+      }, */
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Messenger Clone',
+        short_name: 'Messenger',
+        description: 'Secure Real-Time Messaging',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
